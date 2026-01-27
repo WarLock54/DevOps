45cdb86155b31eddf329a0de6c4f49c295d761b1
WarLock54 <onr_540@hotmail.com>
2026-01-27 20:24:15 +0300

    fdgd

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
-  value = azurerm_resource_group.rg.id
+  value       = azurerm_resource_group.rg.id
+  description = "The ID of the Resource Group"
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
+  description = "The primary blob endpoint of the storage account"
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  value       = azurerm_virtual_network.vnet.id
+  description = "The ID of the Virtual Network"
 }
