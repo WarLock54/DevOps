65ab9fa76dc7770ea31aca01dea61f1ebe1aa740
WarLock54 <onr_540@hotmail.com>
2026-01-28 00:21:45 +0300

    change

--- task04/variables.tf
+++ task04/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" { default = "cmaz-12nxowyz-mod4-rg" }
-variable "vnet_name"           { default = "cmaz-12nxowyz-mod4-vnet" }
-variable "subnet_name"         { default = "frontend" }
-variable "nic_name"            { default = "cmaz-12nxowyz-mod4-nic" }
-variable "nsg_name"            { default = "cmaz-12nxowyz-mod4-nsg" }
-variable "pip_name"            { default = "cmaz-12nxowyz-mod4-pip" }
-variable "vm_name"             { default = "cmaz-12nxowyz-mod4-vm" }
-variable "dns_label"           { default = "cmaz-12nxowyz-mod4-nginx" }
+variable "vnet_name" { default = "cmaz-12nxowyz-mod4-vnet" }
+variable "subnet_name" { default = "frontend" }
+variable "nic_name" { default = "cmaz-12nxowyz-mod4-nic" }
+variable "nsg_name" { default = "cmaz-12nxowyz-mod4-nsg" }
+variable "pip_name" { default = "cmaz-12nxowyz-mod4-pip" }
+variable "vm_name" { default = "cmaz-12nxowyz-mod4-vm" }
+variable "dns_label" { default = "cmaz-12nxowyz-mod4-nginx" }
 
 variable "vm_password" {
   type      = string
@@ -13,7 +13,7 @@ variable "vm_password" {
 }
 
 variable "tags" {
-  type    = map(string)
+  type = map(string)
   default = {
     Creator = "onur_atalik@epam.com"
   }
