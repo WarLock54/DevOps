4193170af6c62e70a44f8d5866e8f1b6f0a4a0b9
WarLock54 <onr_540@hotmail.com>
2026-01-27 23:58:27 +0300

    task04

---
+++ task04/main.tf
@@ -0,0 +1,136 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "East US"
+  tags     = var.tags
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+# Standalone Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Public IP
+# Public IP (SKU Standard olarak güncellendi)
+resource "azurerm_public_ip" "pip" {
+  name                = var.pip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = var.dns_label
+  tags                = var.tags
+}
+
+# NSG
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+# Standalone NSG Rule: SSH
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "AllowSSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# Standalone NSG Rule: HTTP
+resource "azurerm_network_security_rule" "http" {
+  name                        = "AllowHTTP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# NIC
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Standalone NIC-NSG Association
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B2s_v2"
+  admin_username                  = "azureuser"
+  admin_password                  = var.vm_password
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  tags                            = var.tags
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  # Provisioner to install Nginx
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = self.admin_username
+      password = self.admin_password
+      host     = self.public_ip_address
+    }
+  }
+}
---
+++ task04/variables.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" { default = "cmaz-12nxowyz-mod4-rg" }
+variable "vnet_name"           { default = "cmaz-12nxowyz-mod4-vnet" }
+variable "subnet_name"         { default = "frontend" }
+variable "nic_name"            { default = "cmaz-12nxowyz-mod4-nic" }
+variable "nsg_name"            { default = "cmaz-12nxowyz-mod4-nsg" }
+variable "pip_name"            { default = "cmaz-12nxowyz-mod4-pip" }
+variable "vm_name"             { default = "cmaz-12nxowyz-mod4-vm" }
+variable "dns_label"           { default = "cmaz-12nxowyz-mod4-nginx" }
+
+variable "vm_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {
+    Creator = "onur_atalik@epam.com"
+  }
+}
