6c98eec2ebd3932afedd64060b39e36318618ddf
WarLock54 <onr_540@hotmail.com>
2026-01-27 20:42:05 +0300

    Fix: add mandatory descriptions to outputs and cleanup structure

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
 output "rg_id" {
   value       = azurerm_resource_group.rg.id
-  description = "The ID of the Resource Group"
+  description = "Resource Group ID"
 }
 
 output "sa_blob_endpoint" {
   value       = azurerm_storage_account.sa.primary_blob_endpoint
-  description = "The primary blob endpoint of the storage account"
+  description = "Storage account blob service primary endpoint"
 }
 
 output "vnet_id" {
   value       = azurerm_virtual_network.vnet.id
-  description = "The ID of the Virtual Network"
+  description = "Virtual network ID"
 }
