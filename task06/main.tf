5155208fd56c69d03e072669b3880ab2c659d943
WarLock54 <onr_540@hotmail.com>
2026-02-01 16:56:19 +0300

    pls

--- task06/main.tf
+++ task06/main.tf
@@ -1,23 +1,7 @@
-resource "azurerm_key_vault" "main" {
-  name                        = var.azurerm_key_vault_name
-  location                    = azurerm_resource_group.main.location
-  resource_group_name         = azurerm_resource_group.main.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name                    = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Get", "List", "Set", "Delete", "Purge", "Recover"
-    ]
-  }
+data "azurerm_key_vault" "existing" {
+  name                = var.kv_name
+  resource_group_name = var.kv_rg_name
 }
-data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "main" {
   name     = local.rg_name
@@ -35,7 +19,7 @@ module "sql" {
   sql_admin_username        = var.sql_admin_username
   sql_admin_secret_name     = var.sql_admin_secret_name
   sql_admin_secret_password = var.sql_admin_secret_password
-  key_vault_id              = azurerm_key_vault.main.id
+  key_vault_id              = data.azurerm_key_vault.existing.id
   allowed_ip_address        = var.allowed_ip_address
   sql_fwr_name              = var.sql_fwr_name
   tags                      = local.common_tags
