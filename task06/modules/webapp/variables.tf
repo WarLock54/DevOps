3c3172320250b8b84b594d30e9f2ee2d280bb064
WarLock54 <onr_540@hotmail.com>
2026-02-01 15:03:28 +0300

    -recursive

--- task06/modules/webapp/variables.tf
+++ task06/modules/webapp/variables.tf
@@ -1,32 +1,32 @@
-variable "location" { 
-    type = string 
-    description = "The Azure region where resources will be deployed."
-    }
-variable "resource_group_name" { 
-    type = string
-    description = "The name of the resource group where resources will be deployed."
-    }
-variable "asp_name" { 
-    type = string
-    description = "The name of the App Service Plan."
-    }
-variable "asp_sku" { 
-    type = string
-    description = "The SKU name for the App Service Plan."
-    }
-variable "app_name" { 
-    type = string 
-    description = "The name of the Linux Web App."
-    }
-variable "app_dotnet_version" { 
-    type = string
-    description = "The .NET version for the Linux Web App."
-     }
+variable "location" {
+  type        = string
+  description = "The Azure region where resources will be deployed."
+}
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group where resources will be deployed."
+}
+variable "asp_name" {
+  type        = string
+  description = "The name of the App Service Plan."
+}
+variable "asp_sku" {
+  type        = string
+  description = "The SKU name for the App Service Plan."
+}
+variable "app_name" {
+  type        = string
+  description = "The name of the Linux Web App."
+}
+variable "app_dotnet_version" {
+  type        = string
+  description = "The .NET version for the Linux Web App."
+}
 variable "sql_connection_string" {
-     type = string
-     description = "The connection string for the SQL database."
-      }
-variable "tags" { 
-    type = map(string)
-    description = "Tags to be applied to the Web App resources."
-     }
+  type        = string
+  description = "The connection string for the SQL database."
+}
+variable "tags" {
+  type        = map(string)
+  description = "Tags to be applied to the Web App resources."
+}
