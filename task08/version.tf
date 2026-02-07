c9eee6225ec777bbb2773819a054dc4924d62895
WarLock54 <onr_540@hotmail.com>
2026-02-07 15:14:51 +0300

    version error

--- task08/version.tf
+++ task08/version.tf
@@ -1,11 +1,9 @@
 terraform {
-  # Testin beklediği >= 1.5.7 kısıtlamasını tam olarak karşılar
   required_version = ">= 1.5.7"
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      # Testin beklediği >= 3.110.0 ve < 4.0.0 aralığını tam olarak karşılar
+      source  = "hashicorp/azurerm"
       version = ">= 3.110.0, < 4.0.0"
     }
     kubernetes = {
@@ -18,9 +16,11 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
+
 provider "kubernetes" {
   host                   = module.aks.host
   client_certificate     = base64decode(module.aks.client_certificate)
@@ -28,7 +28,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
 }
 
-# Kubectl sağlayıcısını AKS verileriyle yapılandırın
 provider "kubectl" {
   host                   = module.aks.host
   client_certificate     = base64decode(module.aks.client_certificate)
