0a9d7b433bca4d8ef071a0d512ebff4f92d5b5a4
WarLock54 <onr_540@hotmail.com>
2026-02-07 14:22:00 +0300

    lasttask8

--- task08/modules/redis/variables.tf
+++ task08/modules/redis/variables.tf
@@ -1,10 +1,40 @@
-variable "name" { description = "Redis instance name" }
-variable "location" { description = "Azure region" }
-variable "rg_name" { description = "Resource group name" }
-variable "capacity" { description = "Redis capacity" }
-variable "sku_family" { description = "Redis SKU family" }
-variable "sku" { description = "Redis SKU name" }
-variable "kv_id" { description = "Key Vault ID to store secrets" }
-variable "secret_name_hostname" { description = "Secret name for hostname" }
-variable "secret_name_key" { description = "Secret name for primary key" }
-variable "tags" { type = map(string) }
+variable "name" {
+  type        = string
+  description = "Redis instance name"
+}
+variable "location" {
+  type        = string
+  description = "Azure region"
+}
+variable "rg_name" {
+  type        = string
+  description = "Resource group name"
+}
+variable "capacity" {
+  type        = number
+  description = "Redis capacity"
+}
+variable "sku_family" {
+  type        = string
+  description = "Redis SKU family"
+}
+variable "sku" {
+  type        = string
+  description = "Redis SKU name"
+}
+variable "kv_id" {
+  type        = string
+  description = "Key Vault ID to store secrets"
+}
+variable "secret_name_hostname" {
+  type        = string
+  description = "Secret name for hostname"
+}
+variable "secret_name_key" {
+  type        = string
+  description = "Secret name for primary key"
+}
+variable "tags" {
+  type        = map(string)
+  description = "Common tags"
+}
