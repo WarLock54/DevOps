16343e9ba6d20ca424fbb8aa7ecaa8295fe185c3
WarLock54 <onr_540@hotmail.com>
2026-02-08 15:13:30 +0300

    local

--- task08/locals.tf
+++ task08/locals.tf
@@ -1,13 +1,10 @@
 locals {
-  rg_name       = "${var.resources_name_prefix}-rg"
-  redis_name    = "${var.resources_name_prefix}-redis"
-  keyvault_name = "${var.resources_name_prefix}-kv"
-
-  acr_name = "cmtr12nxowyzmod8cr"
-
-  aks_name = "${var.resources_name_prefix}-aks"
-
-  aci_name = "${var.resources_name_prefix}-ci"
+  rg_name       = "${var.resources_name_prefix}-rg"    # cmtr-12nxowyz-mod8-rg
+  redis_name    = "${var.resources_name_prefix}-redis" # cmtr-12nxowyz-mod8-redis
+  keyvault_name = "${var.resources_name_prefix}-kv"    # cmtr-12nxowyz-mod8-kv
+  acr_name      = replace("${var.resources_name_prefix}cr", "-", "")
+  aks_name      = "${var.resources_name_prefix}-aks" # cmtr-12nxowyz-mod8-aks
+  aci_name      = "${var.resources_name_prefix}-ci"  # cmtr-12nxowyz-mod8-ci
 
   common_tags = {
     Creator = var.student_email
