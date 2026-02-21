0d8b903e08d0ba92ce598d2902f09c5a98d3e4f4
WarLock54 <onr_540@hotmail.com>
2026-02-21 14:26:19 +0300

    cfvgbhnj

--- task09/locals.tf
+++ task09/locals.tf
@@ -1,6 +1,15 @@
 locals {
-  afw_name  = "${var.unique_id}-afw"
-  pip_name  = "${var.unique_id}-pip"
-  rt_name   = "${var.unique_id}-rt"
-  snet_name = "AzureFirewallSubnet"
+  # Azure Resource Abbreviations
+  afw_name  = "${var.unique_id}-afw" # Azure Firewall
+  pip_name  = "${var.unique_id}-pip" # Public IP
+  rt_name   = "${var.unique_id}-rt"  # Route Table
+  snet_name = "AzureFirewallSubnet"  # Required name for Firewall subnet
+
+  # Common tags for all resources
+  common_tags = {
+    Environment = "Training"
+    Project     = "Task09"
+    ManagedBy   = "Terraform"
+    Owner       = "cmtr-12nxowyz-mod9"
+  }
 }
