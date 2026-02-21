26f34ede0a9afbce153d1039e411a4ca16500412
WarLock54 <onr_540@hotmail.com>
2026-02-21 17:09:04 +0300

    asdasd

--- task09/outputs.tf
+++ task09/outputs.tf
@@ -1,9 +1,7 @@
 output "azure_firewall_public_ip" {
-  description = "Azure Firewall Public IP address"
-  value       = module.azure_firewall.public_ip
+  value = module.azure_firewall.public_ip
 }
 
 output "azure_firewall_private_ip" {
-  description = "Azure Firewall Private IP address"
-  value       = module.azure_firewall.private_ip
+  value = module.azure_firewall.private_ip
 }
